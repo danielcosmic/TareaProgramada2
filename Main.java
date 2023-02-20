f4a28239cc752b85d0fd7e4b2f93ab3b4ca34e2f
Raquel <123610323+rxquelrojxs@users.noreply.github.com>
2023-02-19 23:27:39 -0600

    x

--- Main.java
+++ Main.java
@@ -43,101 +43,12 @@ public class Main{
             System.out.println("-Ejemplo de coordenada: 1,2");
             System.out.println(" ");
 
-            do{
-                //for para jugar 6 partidas y luego preguntar si se desea guardar la partida
-                for (int i=0; i<6; i++){
-
-                    //turno jugador1
-                    System.out.println(jug1.getNombre() + ", ingrese una coordenada:");
-                    String c1 = in.nextLine();
-                    int[] coordenada1 = convertirCoordenada(c1);
-                    System.out.println("El numero que se encuentra en esa posicion es " + tablero[coordenada1[0]][coordenada1[1]]);
-                    //if para verificar si es par, impar o si ya se escogio antes
-                    if (tablero[coordenada1[0]][coordenada1[1]] < 0){
-                        //ya se escogio esa posicion antes
-                    }else if ((tablero[coordenada1[0]][coordenada1[1]] > 0) && (((tablero[coordenada1[0]][coordenada1[1]] % 2) != 0) && (tablero[coordenada1[0]][coordenada1[1]]!=0))){
-                        tablero[coordenada1[0]][coordenada1[1]] = -1;
-                    }else if ((tablero[coordenada1[0]][coordenada1[1]] > 0) && (((tablero[coordenada1[0]][coordenada1[1]] % 2) == 0))){
-                        tablero[coordenada1[0]][coordenada1[1]] = -3;
-                        fallosJ1+=1;
-                        //PRUEBA
-                        System.out.println("fallos: " + fallosJ1);
-                    }else{
-                        tablero[coordenada1[0]][coordenada1[1]] = -3;
-                        fallosJ1+=1;
-                        //PRUEBA
-                        System.out.println("fallos: " + fallosJ1);
-                    }// fin todos los if -----------------------------------
-                    if(fallosJ1>=5){
-                        break;
-                    }//fin if
-
-                    //turno jugador2
-                    System.out.println(jug2.getNombre() + ", ingrese una coordenada:");
-                    String c2 = in.nextLine();
-                    int[] coordenada2 = convertirCoordenada(c2);
-                    System.out.println("El numero que se encuentra en esa posicion es " + tablero[coordenada2[0]][coordenada2[1]]);
-                    //if para verificar si es par, impar o si ya se escogio antes
-                    if (tablero[coordenada2[0]][coordenada2[1]] < 0){
-                        //ya se escogio esa posicion antes
-                    }else if ((tablero[coordenada2[0]][coordenada2[1]] > 0) && (((tablero[coordenada2[0]][coordenada2[1]] % 2) == 0) || (tablero[coordenada2[0]][coordenada2[1]] == 0))){
-                        tablero[coordenada2[0]][coordenada2[1]] = -2;
-                    }else if ((tablero[coordenada2[0]][coordenada2[1]] > 0) && (((tablero[coordenada2[0]][coordenada2[1]] % 2) != 0) && (tablero[coordenada2[0]][coordenada2[1]]!=0))){
-                        tablero[coordenada2[0]][coordenada2[1]] = -3;
-                        fallosJ2+=1;
-                        //PRUEBA
-                        System.out.println("fallos: " + fallosJ2);
-                    }// fin todos los if -----------------------------------
-                    if(fallosJ2>=5){
-                        break;
-                    }//fin if
-                }//fin for
-
-                if((fallosJ1>=5)||(fallosJ2>=5)){
-                    if(fallosJ1>=5){
-                        System.out.println("Fin de la partida");
-                        System.out.println("El ganador es: " + jug2.getNombre());
-                        break;
-                    }else{
-                        System.out.println("Fin de la partida");
-                        System.out.println("El ganador es: " + jug1.getNombre());
-                        break;
-                    }//fin if
-                }//fin if
-
-                if(!verificarTablero(tablero)){
-                    if(fallosJ1<fallosJ2){
-                        System.out.println("Fin de la partida");
-                        System.out.println("El ganador es: " + jug1.getNombre());
-                        break;
-                    }else if(fallosJ2<fallosJ1){
-                        System.out.println("Fin de la partida");
-                        System.out.println("El ganador es: " + jug2.getNombre());
-                        break;
-                    }else if(fallosJ2==fallosJ1){
-                        System.out.println("Fin de la partida");
-                        System.out.println("Empate! Ambos ganan");
-                        break;
-                    }
-                }//fin if
-
-                //guardar partida
-                System.out.println("Desea guardar la partida? Ingrese 1 para guardar la partida o 2 para continuar jugando sin guardarla");
-                String opcionG = in.nextLine();
-                if (opcionG.equals("1")){
-                    Archivo.guardarTableroEnCSV(tablero, "Tablero");
-                    Archivo.guardarDatosJugadores("Jugadores", jug1, jug2, fallosJ1, fallosJ2);
-                    System.out.println("Partida guardada");
-                }else if(opcionG.equals("2")){
-                    //sigue
-                }else{
-                    System.out.println("Opcion incorrecta");
-                }
-            }while ( (verificarTablero(tablero)) && (fallosJ1<5) && (fallosJ2<5) );
+            jugarPartida(tablero, jug1, jug2, fallosJ1, fallosJ2);
 
         }else if (opcionP.equals("2")){
             Archivo.cargarDatosGuardados("Jugadores");
             Archivo.cargarTableroGuardado("Tablero");
+            jugarPartida(tablero, jug1, jug2, fallosJ1, fallosJ2);
         }else{
             System.out.println("Opción Inválida.");
         }
@@ -172,5 +83,101 @@ public class Main{
             coordenada[1] = y-1;
         return coordenada;
     }//fin metodo convertirCoordenada
+
+    //metodo para jugar las partidas
+    public static void jugarPartida (int[][] tablero, Jugador jug1, Jugador jug2, int fallosJ1, int fallosJ2){
+        Scanner in = new Scanner(System.in);
+        do{
+            //for para jugar 6 partidas y luego preguntar si se desea guardar la partida
+            for (int i=0; i<6; i++){
+
+                //turno jugador1
+                System.out.println(jug1.getNombre() + ", ingrese una coordenada:");
+                String c1 = in.nextLine();
+                int[] coordenada1 = convertirCoordenada(c1);
+                System.out.println("El numero que se encuentra en esa posicion es " + tablero[coordenada1[0]][coordenada1[1]]);
+                //if para verificar si es par, impar o si ya se escogio antes
+                if (tablero[coordenada1[0]][coordenada1[1]] < 0){
+                    //ya se escogio esa posicion antes
+                }else if ((tablero[coordenada1[0]][coordenada1[1]] > 0) && (((tablero[coordenada1[0]][coordenada1[1]] % 2) != 0) && (tablero[coordenada1[0]][coordenada1[1]]!=0))){
+                    tablero[coordenada1[0]][coordenada1[1]] = -1;
+                }else if ((tablero[coordenada1[0]][coordenada1[1]] > 0) && (((tablero[coordenada1[0]][coordenada1[1]] % 2) == 0))){
+                    tablero[coordenada1[0]][coordenada1[1]] = -3;
+                    fallosJ1+=1;
+                    //PRUEBA
+                    System.out.println("fallos: " + fallosJ1);
+                }else{
+                    tablero[coordenada1[0]][coordenada1[1]] = -3;
+                    fallosJ1+=1;
+                    //PRUEBA
+                    System.out.println("fallos: " + fallosJ1);
+                }// fin todos los if -----------------------------------
+                if(fallosJ1>=5){
+                    break;
+                }//fin if
+
+                //turno jugador2
+                System.out.println(jug2.getNombre() + ", ingrese una coordenada:");
+                String c2 = in.nextLine();
+                int[] coordenada2 = convertirCoordenada(c2);
+                System.out.println("El numero que se encuentra en esa posicion es " + tablero[coordenada2[0]][coordenada2[1]]);
+                //if para verificar si es par, impar o si ya se escogio antes
+                if (tablero[coordenada2[0]][coordenada2[1]] < 0){
+                    //ya se escogio esa posicion antes
+                }else if ((tablero[coordenada2[0]][coordenada2[1]] > 0) && (((tablero[coordenada2[0]][coordenada2[1]] % 2) == 0) || (tablero[coordenada2[0]][coordenada2[1]] == 0))){
+                    tablero[coordenada2[0]][coordenada2[1]] = -2;
+                }else if ((tablero[coordenada2[0]][coordenada2[1]] > 0) && (((tablero[coordenada2[0]][coordenada2[1]] % 2) != 0) && (tablero[coordenada2[0]][coordenada2[1]]!=0))){
+                    tablero[coordenada2[0]][coordenada2[1]] = -3;
+                    fallosJ2+=1;
+                    //PRUEBA
+                    System.out.println("fallos: " + fallosJ2);
+                }// fin todos los if -----------------------------------
+                if(fallosJ2>=5){
+                    break;
+                }//fin if
+            }//fin for
+
+            if((fallosJ1>=5)||(fallosJ2>=5)){
+                if(fallosJ1>=5){
+                    System.out.println("Fin de la partida");
+                    System.out.println("El ganador es: " + jug2.getNombre());
+                    break;
+                }else{
+                    System.out.println("Fin de la partida");
+                    System.out.println("El ganador es: " + jug1.getNombre());
+                    break;
+                }//fin if
+            }//fin if
+
+            if(!verificarTablero(tablero)){
+                if(fallosJ1<fallosJ2){
+                    System.out.println("Fin de la partida");
+                    System.out.println("El ganador es: " + jug1.getNombre());
+                    break;
+                }else if(fallosJ2<fallosJ1){
+                    System.out.println("Fin de la partida");
+                    System.out.println("El ganador es: " + jug2.getNombre());
+                    break;
+                }else if(fallosJ2==fallosJ1){
+                    System.out.println("Fin de la partida");
+                    System.out.println("Empate! Ambos ganan");
+                    break;
+                }
+            }//fin if
+
+            //guardar partida
+            System.out.println("Desea guardar la partida? Ingrese 1 para guardar la partida o 2 para continuar jugando sin guardarla");
+            String opcionG = in.nextLine();
+            if (opcionG.equals("1")){
+                Archivo.guardarTableroEnCSV(tablero, "Tablero");
+                Archivo.guardarDatosJugadores("Jugadores", jug1, jug2, fallosJ1, fallosJ2);
+                System.out.println("Partida guardada");
+            }else if(opcionG.equals("2")){
+                //sigue
+            }else{
+                System.out.println("Opcion incorrecta");
+            }
+        }while ( (verificarTablero(tablero)) && (fallosJ1<5) && (fallosJ2<5) );
+    }//fin metodo jugarPartida
 
 }//fin clase
